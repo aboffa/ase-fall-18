25bbdca2ab78b000d3690ea3a65832519fd0bff9
aboffa <a.boffa95@gmail.com>
2018-09-28 10:22:54 +0200

    first commit

---
+++ Lab_1/calculator.py
@@ -0,0 +1,29 @@
+#calculator.py 
+
+def sum(m,n):
+    result=m
+    if n > 0:
+        for i in range(n):
+            result+=1
+    else:
+        for i in range(abs(n)):
+            result-=1
+    return result
+
+def divide(m,n):
+    result=0
+    negativeResult=False
+    if n < 0:
+        n*=-1
+        negativeResult = not negativeResult
+    if m < 0: 
+        m*=-1
+        negativeResult = not negativeResult
+    while((m-n)>=0):
+        m-=n
+        result+=1
+    result = -result if negativeResult else result
+    return result
+
+#print(sum(-10,-2))
+#print(divide(10,-1))
